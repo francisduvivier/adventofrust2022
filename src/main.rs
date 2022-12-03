913f1e2aaab961ce4a3cb3ea053e726700039040
Francis Duvivier <francisduvivier@gmail.com>
2022-12-03 23:08:40 +0100

    Start day3b

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod day3a;
 mod util;
+mod day3b;
 
 pub use util::*;
 
 fn main() {
     println!("Hello, world!");
-    println!("Solution: {}", day3a::solve(read_input(3)));
+    // println!("Solution: {}", day3a::solve(read_input(3)));
+    println!("Solution: {}", day3b::solve(read_input(3)));
 }
 
 
