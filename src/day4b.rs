7139b7cefef24d373e45f4cbc87c4a97028fc936
Francis Duvivier <francisduvivier@gmail.com>
2022-12-04 11:16:22 +0100

    Solve day4b

---
+++ src/day4b.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs;
+use std::str::Split;
+
+pub fn solve(input_lines: Vec<String>) -> String {
+    println!("nb lines: {}", input_lines.len());
+    let mut count = 0;
+    for line in input_lines {
+        // println!("line: {}", line);
+        count += line_score(line);
+    }
+    return count.to_string();
+}
+
+fn line_score(line: String) -> i32 {
+    let number_pairs = line.split(',').map(|numbers| numbers.split('-').collect::<Vec<&str>>()).collect::<Vec<Vec<&str>>>();
+    // println!("sizes {}-{}", left.len(), right.len());
+    let lower_left: i32 = number_pairs[0][0].parse().unwrap();
+    let upper_left: i32 = number_pairs[0][1].parse().unwrap();
+    let lower_right: i32 = number_pairs[1][0].parse().unwrap();
+    let upper_right: i32 = number_pairs[1][1].parse().unwrap();
+    if (lower_right >= lower_left && upper_left >= lower_right) {
+        return 1;
+    }
+    if (lower_left >= lower_right && upper_right >= lower_left) {
+        return 1;
+    }
+    return 0;
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day3a;
 mod util;
 mod day3b;
 mod day4a;
+mod day4b;
 
 pub use util::*;
 
@@ -9,7 +10,8 @@ fn main() {
     println!("Hello, world!");
     // println!("Solution: {}", day3a::solve(read_input(3)));
     // println!("Solution: {}", day3b::solve(read_input(3)));
-    println!("Solution: {}", day4a::solve(read_input(4)));
+    // println!("Solution: {}", day4a::solve(read_input(4)));
+    println!("Solution: {}", day4b::solve(read_input(4)));
 }
 
 
