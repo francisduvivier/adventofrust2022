ff2fab377f1fa913d03b0ac36af563ba46360197
Francis Duvivier <francisduvivier@gmail.com>
2022-12-07 20:46:42 +0100

    Solve day5 part2

---
+++ src/day5b.rs
@@ -0,0 +1,74 @@
+use std::{env, str};
+use std::fs;
+use std::str::Split;
+use std::str::from_utf8;
+use regex::Regex;
+use substring::Substring;
+
+pub fn solve(input_lines: Vec<String>) -> String {
+    let debug = false;
+    println!("nb lines: {}", input_lines.len());
+    let mut stacks: Vec<Vec<String>> = vec![vec![]; 9];
+    let mut moves: Vec<Move> = vec![];
+    for line in input_lines {
+        if Regex::new(r"^(\[| {4})").unwrap().is_match(line.as_str()) {
+            let chunks = line.as_bytes().chunks(4);
+            let mut i = 0;
+            for chunk in chunks {
+                let chunk_string = from_utf8(chunk).unwrap();
+                if chunk_string.starts_with("[") {
+                    let letter = chunk_string.substring(1, 2);
+                    stacks[i].push(letter.to_string());
+                    if debug { println!("stacks[{}][{}].unwrap(): {}", i, (stacks[i].len() - 1), stacks[i].last().unwrap()); }
+                }
+                i += 1;
+            }
+        } else if line.starts_with("move") {
+            let mv = parse_move(line);
+            if debug { println!("Move {} from {} to {}", mv.amount, mv.from, mv.to); }
+            moves.push(mv);
+        } else if !line.is_empty() && !line.starts_with(" ") {
+            print!("l {}", line);
+            assert!(false);
+        }
+    }
+    println!("moves {}, stacks {}", moves.len(), stacks.len());
+    for mut i in 0..stacks.len() {
+        if debug && !stacks[i].is_empty() { println!("stacks last before [{}]", stacks[i].last().unwrap()); }
+        stacks[i].reverse();
+        if debug && !stacks[i].is_empty() { println!("stacks first after [{}]", stacks[i].first().unwrap()); }
+    }
+    for mv in moves {
+        if debug { println!("DO Move {} from {} to {}", mv.amount, mv.from, mv.to); }
+        let origToSize = stacks[(mv.to - 1) as usize].len();
+        for _ in 0..mv.amount {
+            let popped = stacks[(mv.from - 1) as usize].pop().unwrap();
+            if debug { println!("popped {} from {} to {}", popped, mv.from, mv.to) }
+            stacks[(mv.to - 1) as usize].insert(origToSize, popped);
+        }
+    }
+    let mut chars: Vec<&str> = vec![];
+    for mut i in 0..stacks.len() {
+        if !stacks[i].is_empty() {
+            chars.push(stacks[i].last().unwrap())
+        }
+    }
+    return chars.join("");
+}
+
+struct Move {
+    amount: u8,
+    from: u8,
+    to: u8,
+}
+
+fn parse_move(line: String) -> Move {
+    //eg. move 3 from 5 to 4
+    let matcher = Regex::new(r"^move (\d+) from (\d+) to (\d+)").unwrap();
+    for caps in matcher.captures_iter(&line) {
+        let mv = Move { amount: caps[1].parse().unwrap(), from: caps[2].parse().unwrap(), to: caps[3].parse().unwrap() };
+        return mv;
+    }
+    return Move { amount: 0, from: 0, to: 0 };
+}
+
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day3b;
 mod day4a;
 mod day4b;
 mod day5a;
+mod day5b;
 
 pub use util::*;
 
@@ -13,7 +14,8 @@ fn main() {
     // println!("Solution: {}", day3b::solve(read_input(3)));
     // println!("Solution: {}", day4a::solve(read_input(4)));
     // println!("Solution: {}", day4b::solve(read_input(4)));
-    println!("Solution: {}", day5a::solve(read_input(5)));
+    // println!("Solution: {}", day5a::solve(read_input(5)));
+    println!("Solution: {}", day5b::solve(read_input(5)));
 }
 
 
