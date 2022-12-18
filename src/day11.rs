5a2c00eaa33cc29af9256316d40b7b9de466d7b3
Francis Duvivier <francisduvivier@gmail.com>
2022-12-18 14:34:56 +0100

    Start implementing parsing and cycle execution on day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 enum Operator {
     ADD,
     MULTIPLY,
@@ -5,7 +7,14 @@ enum Operator {
 
 struct Operation {
     operator: Operator,
-    operand: u64,
+    operand: u32,
+}
+
+fn parse_operation(line: &str) -> Operation {
+    return Operation {
+        operator: Operator::ADD, // TODO implement
+        operand: 5, // TODO implement
+    };
 }
 
 struct Monkey {
@@ -14,17 +23,54 @@ struct Monkey {
     test_divider: u32,
     result_monkey_true: u32,
     result_monkey_false: u32,
-    nb_inspections: u32,
+    nb_inspections: u64,
     items: Vec<u64>,
 }
 
-pub fn solve(input_lines: Vec<String>, cycles: usize) -> i32 {
-    let monkeys: Vec<Monkey> = parse_monkeys(input_lines);
+impl FromStr for Monkey {
+    type Err = std::num::ParseIntError;
+
+    // Parses a color hex code of the form '#rRgGbB..' into an
+    // instance of 'RGB'
+    fn from_str(description: &str) -> Result<Self, Self::Err> {
+        let items = vec![1];
+        Ok(Monkey {
+            id: 0,
+            op: parse_operation(""),
+            test_divider: 0,
+            result_monkey_true: 0,
+            result_monkey_false: 0,
+            nb_inspections: 0,
+            items,
+        })
+    }
+}
+
+pub fn solve(input_lines: Vec<String>, cycles: usize) -> u64 {
+    let mut monkeys: Vec<Monkey> = parse_monkeys(input_lines);
     for i in 0..cycles {
-        for monkey in monkeys {
-            // TODO execute action, increase inspections, do test and pass item on
+        for monkey in &mut monkeys {
+            for item in &monkey.items {
+                // TODO execute action, do test and pass item on
+                monkey.nb_inspections += 1;
+            }
         }
     }
-    let sorted_scores = monkeys.map(|m| m.nb_inspections).collect().sort();
-    return sorted_scores[0] * sorted_scores[1];
+    monkeys.sort_by(|m1, m2| m2.nb_inspections.cmp(&m1.nb_inspections));
+    return monkeys[0].nb_inspections * monkeys[1].nb_inspections;
+}
+
+fn parse_monkeys(input: Vec<String>) -> Vec<Monkey> {
+    let mut monkeys: Vec<Monkey> = vec![];
+    let joined = input.join(",");
+    let resplit = joined.split("Monkey ");
+    for monkey_description in resplit {
+        println!("monkey {}", monkey_description);
+        monkeys.push(parse_monkey(monkey_description));
+    }
+    monkeys
+}
+
+fn parse_monkey(descriptions: &str) -> Monkey {
+    Monkey::from_str(descriptions).unwrap()
 }
